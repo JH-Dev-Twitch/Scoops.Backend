06e76f3b81269ca6f582f3240e84608455c0a367
JH-Dev-Twitch <jhdevtwitch@gmail.com>
2020-10-23 02:11:17 +0100

    added auth starter project,

---
+++ src/scoops-backend/scoops-env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\repos\scoops.backend\src\scoops-backend\scoops-env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
